93cca36e42bba4a03c56f74b141fc7df1ced260d
Justin Joyce <justin.joyce@joocer.com>
2023-04-21 23:16:33 +0100

    WIP

---
+++ mesos/__main__.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+A command line interface for Mesos
+"""
+import asyncio
+import os
+import sys
+
+import mesos
+import typer
+
+sys.path.insert(1, os.path.join(sys.path[0], "../../opteryx"))
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+
+from mysql_mimic import MysqlServer  # isort: skip
+from mysql_mimic import Session  # isort: skip
+from orso import logging  # isort: skip
+
+logging.set_log_name("MESOS")
+logger = logging.get_logger()
+logger.setLevel(5)
+
+# Define ANSI color codes
+ANSI_RED = "\u001b[31m"
+ANSI_BLUE = "\u001b[36m"
+ANSI_RESET = "\u001b[0m"
+
+
+class MySession(Session):
+    async def query(self, sql, attrs):
+        import opteryx  # isort: skip
+
+        print(f"Original SQL string: {sql}")
+        print(f"Query attributes: {attrs}")
+        print(f"Currently authenticated user: {self.username}")
+        print(f"Currently selected database: {self.database}")
+
+        try:
+            curr = opteryx.query(sql)
+            return curr.fetchall(), curr.column_names
+        except Exception as err:
+            logger.exception(f"{type(err).__name__} - {err}")
+            return False
+
+
+# fmt:off
+def main():
+# fmt:on
+    print(f"{ANSI_BLUE}   ____ ___  ___  _________  _____")
+    print("  / __ `__ \/ _ \/ ___/ __ \/ ___/")
+    print(" / / / / / /  __(__  ) /_/ (__  ) ")
+    print(f"/_/ /_/ /_/\___/____/\____/____/  {ANSI_RESET}")
+    print(f"     mesos version {mesos.__version__}")
+    server = MysqlServer(session_factory=MySession, port=3306)
+    asyncio.run(server.serve_forever())
+
+
+if __name__ == "__main__":  # pragma: no cover
+    try:
+        typer.run(main)
+    except Exception as e:
+        # Display a friendly error message if an exception occurs
+        print(f"{ANSI_RED}Error{ANSI_RESET}: {e}")
--- setup.py
+++ setup.py
@@ -1,47 +1,38 @@
 from setuptools import find_packages
 from setuptools import setup
 
-# Import __version__
-exec(open("mysql_mimic/version.py").read())
+LIBRARY = "mesos"
 
-setup(
-    name="Mesos",
-    version=__version__,
-    description="A python implementation of the mysql server protocol",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/kelsin/mysql-mimic",
-    author="Christopher Giroir",
-    author_email="kelsin@valefor.com",
-    license="MIT",
-    packages=find_packages(include=["mysql_mimic", "mysql_mimic.*"]),
-    python_requires=">=3.6",
-    install_requires=[],
-    extras_require={
-        "dev": [
-            "aiomysql",
-            "mypy",
-            "mysql-connector-python",
-            "black",
-            "coverage",
-            "gssapi",
-            "k5test",
-            "pylint",
-            "pytest",
-            "pytest-asyncio",
-            "sphinx",
-            "sqlalchemy",
-            "twine",
-            "wheel",
-        ],
-        "krb5": ["gssapi"],
+__version__ = "notset"
+with open(f"{LIBRARY}/version.py", mode="r") as v:
+    vers = v.read()
+exec(vers)  # nosec
+
+with open("README.md", mode="r", encoding="UTF8") as rm:
+    long_description = rm.read()
+
+try:
+    with open("requirements.txt", "r") as f:
+        required = f.read().splitlines()
+except:
+    with open(f"{LIBRARY}.egg-info/requires.txt", "r") as f:
+        required = f.read().splitlines()
+
+setup_config = {
+    "name": LIBRARY,
+    "version": __version__,
+    "description": "MySQL compatible interface for Opteryx.",
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "maintainer": "@joocer",
+    "author": "@joocer",
+    "author_email": "justin.joyce@joocer.com",
+    "packages": find_packages(include=[LIBRARY, f"{LIBRARY}.*"]),
+    "python_requires": ">=3.8",
+    "install_requires": required,
+    "entry_points": {
+        "console_scripts": ["mesos=mesos.command:main"],
     },
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "Operating System :: OS Independent",
-        "Programming Language :: SQL",
-        "Programming Language :: Python :: 3 :: Only",
-    ],
-)
+}
+
+setup(**setup_config)
